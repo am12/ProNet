fd2be19cf4163418f73c72b4aa5f7a9fb41e7f1c
Alan Mao <52225670+am12@users.noreply.github.com>
2024-04-11 17:28:15 -0400

    update model architecture'

--- src/pronet.py
+++ src/pronet.py
@@ -1,7 +1,8 @@
-from torch.nn import Module, BatchNorm1d, LazyBatchNorm1d, ReLU, LeakyReLU, Conv1d, LazyConv1d, ModuleList, Softmax, Sigmoid, Flatten, Dropout2d, Linear
+from torch.nn import Module, BatchNorm1d, LeakyReLU, Conv1d, ModuleList, Softmax, Linear
 import numpy as np
 
 CARDINALITY_ITEM = 16
+INPUT_CHANNELS = 20
 NUM_CLASSES = 27
 
 class ResidualUnit(Module):
@@ -11,14 +12,13 @@ class ResidualUnit(Module):
         self.batchnorm1 = BatchNorm1d(l)
         self.relu = LeakyReLU(0.1)
         self.batchnorm2 = BatchNorm1d(l)
-        self.C = bot_channels//CARDINALITY_ITEM
+        self.C = bot_channels // CARDINALITY_ITEM
         self.conv1 = Conv1d(l, l, w, dilation=ar, padding=(w-1)*ar//2, groups=self.C)
         self.conv2 = Conv1d(l, l, w, dilation=ar, padding=(w-1)*ar//2, groups=self.C)
 
     def forward(self, x, y):
         x1 = self.relu(self.batchnorm1(self.conv1(x)))
         x2 = self.relu(self.batchnorm2(self.conv2(x1)))
-
         return x + x2, y
 
 class Skip(Module):
@@ -29,12 +29,10 @@ class Skip(Module):
     def forward(self, x, y):
         return x, self.conv(x) + y
 
-
 class ProNet(Module):
     def __init__(self, L=64, W=np.array([11]*8+[21]*4+[41]*4), AR=np.array([1]*4+[4]*4+[10]*4+[25]*4)):
         super().__init__()
-        self.CL = 2 * (AR * (W - 1)).sum()  # context length
-        self.conv1 = Conv1d(4, L, 1)
+        self.conv1 = Conv1d(INPUT_CHANNELS, L, 1) 
         self.skip1 = Skip(L)
         self.residual_blocks = ModuleList()
         for i, (w, r) in enumerate(zip(W, AR)):
@@ -43,14 +41,15 @@ class ProNet(Module):
                 self.residual_blocks.append(Skip(L))
         if (len(W)+1) % 4 != 0:
             self.residual_blocks.append(Skip(L))
-        self.last_cov = Conv1d(L, 3, 1)
-        self.softmax = Softmax(dim=NUM_CLASSES)
+        self.last_cov = Conv1d(L, L, 1)
+        self.final_layer = Linear(L, NUM_CLASSES)  # Change here for 27 classes
+        self.softmax = Softmax(dim=1)  # softmax along the feature dimension
 
     def forward(self, x):
         x, skip = self.skip1(self.conv1(x), 0)
         for m in self.residual_blocks:
             x, skip = m(x, skip)
-        #######################################
-        # predicting pb for every bp
-        #######################################
-        return self.softmax(self.last_cov(skip))
+        x = self.last_cov(skip)
+        x = x.view(x.size(0), -1)  # Flatten the output for the linear layer
+        x = self.final_layer(x)
+        return self.softmax(x)
