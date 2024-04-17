b5e3ae687d7c585648ec5dac10da80a9d37476c3
Alan Mao <52225670+am12@users.noreply.github.com>
2024-04-17 11:44:27 -0400

    training loop through

--- src/2_train.py
+++ src/2_train.py
@@ -21,7 +21,6 @@ input_dir = '../results/1/'
 out_dir = '../results/2/'
 os.makedirs(os.path.dirname(out_dir), exist_ok=True)
 
-
 def same_seeds(seed):
     '''Fix random seeds for PyTorch'''
     torch.manual_seed(seed)
@@ -33,13 +32,13 @@ def same_seeds(seed):
 
 same_seeds(0) # this seed is for torch 
 
-def categorical_crossentropy_2d(y_pred, y_true, seq_weight=5, gamma=2):
-    '''Computes weighted categorical CE loss for 2D tensor, appling focal loss modification for hard examples.'''
-    return - torch.mean(y_true[:, 0, :] * torch.mul( torch.pow( torch.sub(1, y_pred[:, 0, :]), gamma ), torch.log(y_pred[:, 0, :]+1e-10) )
-                        + seq_weight * y_true[:, 1, :] * torch.mul( torch.pow( torch.sub(1, y_pred[:, 1, :]), gamma ), torch.log(y_pred[:, 1, :]+1e-10) )
-                        + seq_weight * y_true[:, 2, :] * torch.mul( torch.pow( torch.sub(1, y_pred[:, 2, :]), gamma ), torch.log(y_pred[:, 2, :]+1e-10) ))
+# def categorical_crossentropy_2d(y_pred, y_true, seq_weight=5, gamma=2):
+#     '''Computes weighted categorical CE loss for 2D tensor, appling focal loss modification for hard examples.'''
+#     return - torch.mean(y_true[:, 0, :] * torch.mul( torch.pow( torch.sub(1, y_pred[:, 0, :]), gamma ), torch.log(y_pred[:, 0, :]+1e-10) )
+#                         + seq_weight * y_true[:, 1, :] * torch.mul( torch.pow( torch.sub(1, y_pred[:, 1, :]), gamma ), torch.log(y_pred[:, 1, :]+1e-10) )
+#                         + seq_weight * y_true[:, 2, :] * torch.mul( torch.pow( torch.sub(1, y_pred[:, 2, :]), gamma ), torch.log(y_pred[:, 2, :]+1e-10) ))
 
-def split_data(datafile, test_ratio=0.15, indices=None, seed=None):
+def split_data(datafile, test_ratio=0.15, indices=None, seed=None, save_loader=False):
     '''Create a training and testing split of the datafiles'''
 
     # read all lines from the input file
@@ -73,14 +72,24 @@ def split_data(datafile, test_ratio=0.15, indices=None, seed=None):
         file.writelines(test_lines)
     print(f'\t[INFO] Written datasets to: {base_dir}.')
 
+    if save_loader:
+        print(f'\t[INFO] Saving dataloaders to PyTorch files...')
+        train_loader_path = f'{base_dir}/train_dataloader.pt'
+        test_loader_path = f'{base_dir}/test_dataloader.pt'
+        torch.save(train_loader, train_loader_path)
+        torch.save(test_loader, test_loader_path)
+        print(f'\t[INFO] Saved.')
+
+        return train_loader_path, test_loader_path
+
     return train_file_path, test_file_path
 
-def train_single_epoch(epoch_num, model, device, train_loader, logger, optimizer, criterion=None):
+
+def train_single_epoch(epoch_num, model, device, train_loader, logger, criterion, optimizer):
     
     model.train()
     print(f'\t[INFO] Model in train mode.')
 
-    running_loss = 0.0
     pbar = tqdm(total=len(train_loader), ncols=0, desc="Train", unit=" step")
     for batch_idx, data in enumerate(train_loader): 
             
@@ -89,14 +98,10 @@ def train_single_epoch(epoch_num, model, device, train_loader, logger, optimizer
         seqs = seqs.to(torch.float32).to(device)
         labels = labels.to(torch.float32).to(device)
         seqs = torch.permute(seqs, (0, 2, 1))
-        print('\n', seqs.shape, labels.shape)
         
         # forward pass
         pred = model(seqs)
-        if criterion:
-            loss = criterion(pred, labels)
-        else:
-            loss = categorical_crossentropy_2d(pred, labels)
+        loss = criterion(pred, labels)
 
         # backward and optimize
         optimizer.zero_grad()
@@ -107,11 +112,7 @@ def train_single_epoch(epoch_num, model, device, train_loader, logger, optimizer
         iter_num = epoch_num * len(train_loader) + batch_idx
         logger.add_scalar('Loss/train', loss.item(), iter_num)
 
-        running_loss += loss.item()
         if (batch_idx + 1) % 10 == 0:
-            print(f'\t[INFO] Epoch {epoch_num + 1}, Batch {batch_idx + 1}, Loss: {running_loss / 10:.6f}')     
-            running_loss = 0.0
-        
             # add histograms of model parameters to inspect their distributions
             for name, weight in model.named_parameters():
                 logger.add_histogram(name, weight, iter_num)
@@ -119,9 +120,9 @@ def train_single_epoch(epoch_num, model, device, train_loader, logger, optimizer
         
         pbar.update(1)
         pbar.set_postfix(
-            batch_id=batch_idx,
-            # idx_train=len(train_loader)*BATCH_SIZE,
-            # loss=f"{batch_loss:.6f}",
+            batch_idx=batch_idx,
+            train_idx=epoch_num * len(train_loader),
+            ce_loss=f"{loss:.6f}",
             # A_auprc = f"{A_auprc:.6f}",
             # D_auprc = f"{D_auprc:.6f}",
             # A_Precision=f"{A_TP/(A_TP+A_FP+1e-6):.6f}",
@@ -142,7 +143,7 @@ def train_single_epoch(epoch_num, model, device, train_loader, logger, optimizer
     # print ("Learning rate: %.5f" % (get_lr(optimizer)))
     # print("\n\n")
 
-def evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file, criterion=None):
+def evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file, criterion):
     
     model.eval()
     print(f'\t[INFO] Model in evaluation mode.')
@@ -152,18 +153,17 @@ def evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file, cri
     with torch.no_grad():
         pbar = tqdm(total=len(test_loader), ncols=0, desc="Test", unit=" step")
         for batch_idx, data in enumerate(test_loader):
+
+            ### PREDICTION ###
             seqs, labels, prot_id, cog_id = data
             seqs = seqs.to(torch.float32).to(device)
             labels = labels.to(torch.float32).to(device)
             seqs = torch.permute(seqs, (0, 2, 1))
-            labels = torch.permute(labels, (0, 2, 1))
             
             pred = model(seqs)
-            if criterion:
-                loss = criterion(pred, labels)
-            else:
-                loss = categorical_crossentropy_2d(pred, labels)
+            loss = criterion(pred, labels)
 
+            ### LOGGING ###
             _, predicted = torch.max(pred.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
@@ -171,8 +171,8 @@ def evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file, cri
             pbar.update(1)
             pbar.set_postfix(
                 batch_id=batch_idx,
-                # idx_test=len(test_loader)*BATCH_SIZE,
-                # loss=f"{batch_loss:.6f}",
+                test_idx=epoch_num * len(test_loader),
+                ce_loss=f"{loss:.6f}",
                 # A_auprc = f"{A_auprc:.6f}",
                 # D_auprc = f"{D_auprc:.6f}",
                 # A_Precision=f"{A_TP/(A_TP+A_FP+1e-6):.6f}",
@@ -193,8 +193,20 @@ def evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file, cri
     # print ("Learning rate: %.5f" % (get_lr(optimizer)))
     # print("\n\n")
 
-def train(train_datafile, test_datafile, train_statsfile, batch_size, lr=1e-3, num_epochs=16, device_str=None):
-    
+def train(train_datafile, test_datafile, train_statsfile, batch_size, lr=1e-3, num_epochs=16, device_str=None, saved_loader=False):
+    '''
+    The training runner function. 
+
+    train_datafile (str) -> path to train data csv file. OR path to train dataloader pt file if saved_loader == True
+    test_datafile (str) -> path to test data csv file. OR path to test dataloader pt file if saved_loader == True
+    train_statsfile (str) -> path to evaluation statistics log file. 
+    batch_size (int) -> size of dataloader batch
+    lr (float) -> learning rate
+    num_epochs (int) -> number of epochs to train
+    device_str (str) -> device name
+    saved_loader (bool) -> whether to directly load the dataloader
+    '''
+
     print('Setting up training variables...')
     start_time = time.time()
 
@@ -206,25 +218,24 @@ def train(train_datafile, test_datafile, train_statsfile, batch_size, lr=1e-3, n
         elif torch.backends.mps.is_available():
             device_str = 'mps'
     device = torch.device(device_str)
-    print(f'\t[Info] Running model in {device_str} mode.', flush=True)
+    print(f'\t[SYS] Running model in {device_str} mode.', flush=True)
 
     # load a new instance of ProNet to train
-    print(f'\t[Info] Initializing new ProNet model...', flush=True)
+    print(f'\t[SYS] Initializing new ProNet model...', flush=True)
     model = ProNet().to(device)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-    print(f'\t[Info] Done initializing model.', flush=True)
+    print(f'\t[SYS] Done initializing model.', flush=True)
 
     # load data to train the model on 
-    print(f'\t[Info] Loading data...', flush=True)
-    # train_loader = get_dataloader(batch_size, 'train', train_datafile, True, seed=RANDOM_SEED)
-    # test_loader = get_dataloader(batch_size, 'test', test_datafile, False, seed=RANDOM_SEED)
-    ### to expedite
-    # torch.save(train_loader, '../results/1/train_dataloader.pt')
-    # torch.save(test_loader, '../results/1/test_dataloader.pt')
-    train_loader = torch.load('../results/1/train_dataloader.pt')
-    test_loader = torch.load('../results/1/test_dataloader.pt')
-    print(f'\t[Info] Done loading data.', flush=True)
+    print(f'\t[SYS] Loading data...', flush=True)
+    if saved_loader: 
+        train_loader = torch.load(train_datafile)
+        test_loader = torch.load(test_datafile)
+    else:
+        train_loader = get_dataloader(batch_size, 'train', train_datafile, True, seed=RANDOM_SEED)
+        test_loader = get_dataloader(batch_size, 'test', test_datafile, False, seed=RANDOM_SEED)  
+    print(f'\t[SYS] Done loading data.', flush=True)
 
     # time after iteration
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -241,18 +252,17 @@ def train(train_datafile, test_datafile, train_statsfile, batch_size, lr=1e-3, n
     with open(train_statsfile, 'w') as stats_file:
         for epoch_num in range(num_epochs):
             print(f'Epoch {epoch_num}/{num_epochs}:')
-            train_single_epoch(epoch_num, model, device, train_loader, logger, optimizer, criterion)
-            evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file)
+            train_single_epoch(epoch_num, model, device, train_loader, logger, criterion, optimizer)
+            evaluate_single_epoch(epoch_num, model, device, test_loader, stats_file, criterion)
             torch.save(model, f'{out_dir}runs/experiment_{experiment_number}/models/pronet_epoch-{epoch_num}.pt')
         
     print("--- %s minutes ---" % ((time.time() - start_time)/60.0))
 
 
 if __name__ == '__main__':
-
-    # train_datafile, test_datafile = split_data(f'{input_dir}dataset.csv')
-    ### TO EXPEDITE 
-    train_datafile = '../results/1/train_data.csv'
-    test_datafile = '../results/1/test_data.csv'
+    use_saved_loader = True
+    # train_data, test_data = split_data(f'{input_dir}dataset.csv', save_loader=use_saved_loader)
+    train_data = '../results/1/train_dataloader.pt'
+    test_data = '../results/1/test_dataloader.pt'
     train_statsfile = f'{out_dir}train_statistics.txt'
-    train(train_datafile, test_datafile, train_statsfile, BATCH_SIZE)
+    train(train_data, test_data, train_statsfile, BATCH_SIZE, saved_loader=use_saved_loader)
